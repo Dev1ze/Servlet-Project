cfa6f6249261508c68a5537b4b29d0a58b33fa12
Артем <iron-man1998@mail.ru>
2024-12-20 19:14:20 +0300

    Реализованы математические операции (+, -, *, /), с проверкой на некоректный символ операции

--- Calculator/src/ru/appline/Servlet.java
+++ Calculator/src/ru/appline/Servlet.java
@@ -6,14 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "Servlet")
+@WebServlet(urlPatterns = "/servlet")
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html: charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.print("Hello world");
     }
 }
---
+++ Calculator/src/ru/appline/ServletAnswer.java
@@ -0,0 +1,45 @@
+package ru.appline;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/math")
+public class ServletAnswer extends HttpServlet {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        StringBuffer stringBuffer = new StringBuffer();
+        String line;
+
+        BufferedReader bufferedReader = request.getReader();
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            stringBuffer.append(line);
+        }
+        JsonObject jsonObject = gson.fromJson(String.valueOf(stringBuffer), JsonObject.class);
+        int value_1 = jsonObject.get("a").getAsInt();
+        int value_2 = jsonObject.get("b").getAsInt();
+        String operation = jsonObject.get("math").getAsString();
+        Calculator calculator = new Calculator(value_1, value_2, operation);
+        int result = calculator.getResult();
+
+        JsonObject responseJson = new JsonObject();
+        responseJson.addProperty("result", result);
+
+        PrintWriter pw = response.getWriter();
+        pw.print(gson.toJson(responseJson));
+
+    }
+}
